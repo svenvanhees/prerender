c6d12e61a2b85f902bc51bfec50b1eb56bdcb9d4
Sven van Hees <sven@scoreagency.nl>
2026-01-14 15:40:42 +0100

    Add remote debugging port to Chrome flags in prerender configuration

--- server.js
+++ server.js
@@ -2,28 +2,21 @@
 require('dotenv').config();
 var prerender = require('./lib');
 
-// var server = prerender();
-//
-// server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.browserForceRestart());
-// // server.use(prerender.blockResources());
-// server.use(prerender.addMetaTags());
-// server.use(prerender.removeScriptTags());
-// server.use(prerender.httpHeaders());
-//
-// server.start();
-
-
-// const prerender = require("prerender");
-
-// const server = prerender({});
-
 const prMemoryCache = require('prerender-memory-cache');
 
 const server = prerender({
-  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
+  chromeFlags: [
+    '--no-sandbox',
+    '--headless',
+    '--disable-gpu',
+    '--remote-debugging-port=9222',
+    '--hide-scrollbars',
+    '--disable-dev-shm-usage',
+  ],
   forwardHeaders: true,
-  chromeLocation: '/usr/bin/chromium-browser'
+  chromeLocation: '/usr/bin/chromium-browser',
+  captureConsoleLog: true,
+  logRequests: true,
 });
 
 const memCache = Number(process.env.MEMORY_CACHE) || 0;
