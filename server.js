a9243d193be59de029170d0511865ca7f730ca81
Sven van Hees <sven@scoreagency.nl>
2026-01-14 14:34:13 +0100

    Add memory cache and S3 cache support for prerender configuration

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+// var server = prerender();
+//
+// server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.browserForceRestart());
+// // server.use(prerender.blockResources());
+// server.use(prerender.addMetaTags());
+// server.use(prerender.removeScriptTags());
+// server.use(prerender.httpHeaders());
+//
+// server.start();
 
-server.use(prerender.sendPrerenderHeader());
-server.use(prerender.browserForceRestart());
-// server.use(prerender.blockResources());
-server.use(prerender.addMetaTags());
-server.use(prerender.removeScriptTags());
+
+// const prerender = require("prerender");
+
+// const server = prerender({});
+
+const prMemoryCache = require('prerender-memory-cache');
+
+const server = prerender({
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
+  forwardHeaders: true,
+  chromeLocation: '/usr/bin/chromium-browser'
+});
+
+const memCache = Number(process.env.MEMORY_CACHE) || 0;
+if (memCache === 1) {
+  server.use(prMemoryCache);
+}
+
+const s3Cache = Number(process.env.S3_CACHE) || 0;
+if (s3Cache === 1) {
+  server.use(require('prerender-aws-s3-cache'));
+}
+
+server.use(prerender.blacklist());
 server.use(prerender.httpHeaders());
+server.use(prerender.removeScriptTags());
 
 server.start();
